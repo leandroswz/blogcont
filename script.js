607a3bb8b5f779f53e6824139259e3949c3928de
leandro Costa <leandrin.costta@gmail.com>
2021-10-19 09:21:46 -0300

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+    let acc = document.getElementsByClassName("accordion");
+    
+    for (i = 0; i < acc.length; i++) {
+        acc[i].onclick = function(){
+            this.classList.toggle("active");
+            this.nextElementSibling.classList.toggle("show");
+        }
+    }
+    
